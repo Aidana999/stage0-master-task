da28c41879dabc30e576534eadfa3a2cf0498ed7
Aidana <49874037+Aidana999@users.noreply.github.com>
2023-05-10 19:02:07 +0400

    Update IntegerDivider.java

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -3,6 +3,11 @@ package com.epam.conditions;
 public class IntegerDivider {
 
     public void printCompletelyDivided(int dividend, int divider) {
+        if (((dividend/divider)*divider)==dividend) {
+	       System.out.println("can be divided completely");
+	   }else{
+	       System.out.println("cannot be divided completely" );
+	   }
     }
 
 }

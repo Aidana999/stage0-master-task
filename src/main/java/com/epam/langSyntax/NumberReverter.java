c7e6cd0b94db86478080a75e61457f94c7b7e210
Aidana <49874037+Aidana999@users.noreply.github.com>
2023-05-11 18:10:39 +0400

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,18 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+         int rev_num = 0;
+        while (number > 0) {
+            rev_num = rev_num * 10 + number % 10;
+            number = number / 10;
+        }
+        return rev_num;
+    }
+
+    public void main(String[] args)
+    {
+        int number = 4562;
+        System.out.println(revert(number));
     }
 
 }

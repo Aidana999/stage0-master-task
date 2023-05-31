c77a82070d52f65a8f4e4847ac578340f488d15a
Aidana <49874037+Aidana999@users.noreply.github.com>
2023-05-31 21:02:21 +0400

    Update Power.java

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -1,13 +1,14 @@
 package com.epam.loops;
 
 public class Power {
-
     public void printPower(int numberToPrint, int power) {
-
+        for (int i = 1; i < power; i++) {
+            numberToPrint *= numberToPrint;
+        }
+        System.out.println(numberToPrint);
     }
 
     public static void main(String[] args) {
         new Power().printPower(10, 3);
     }
-
 }

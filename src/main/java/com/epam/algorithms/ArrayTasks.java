16db2a629616d92a3e16dd5e05606167198bb25f
Aidana <49874037+Aidana999@users.noreply.github.com>
2023-06-18 16:24:54 +0400

    Update ArrayTasks.java

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-        String[] seasons = new String { "winter", "spring", "summer", "autumn" };
+        String[] seasons = { "winter", "spring", "summer", "autumn" };
         return seasons;
     }
 
@@ -106,7 +106,7 @@ public class ArrayTasks {
             arr[i] = 0;
         }
 
-        return numCount;
+        return positiveNum;
     }
 
     /**
@@ -127,7 +127,8 @@ public class ArrayTasks {
                     arr[j + 1] = temp;
                 }
             }
-          }
+         }
+        return arr;
     }
 
 }
